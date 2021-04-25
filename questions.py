ac2da153598873a11510b395a0d0769e722a74d3
THAMIZHMANI-A <you@example.com>
2021-04-25 12:46:55 +0530

    My First Commit

---
+++ questions.py
@@ -0,0 +1,128 @@
+HARS_QUESTIONS = {
+    "ru": {
+        "answers": ["காணவில்லை", "பலவீனமான அளவிற்கு", "மிதமான அளவிற்கு", "கடுமையான அளவிற்கு", "மிகவும் கடுமையான அளவிற்கு"],
+        "questions": [
+            "கவலை மனநிலை. (ஆர்வம், மோசமான எதிர்பார்ப்பு, ஆபத்தான அச்சங்கள், எரிச்சல்)",
+            "மின்னழுத்த. (பதற்றம், நடுக்கம், எளிதில் கண்ணீர், நடுக்கம், பதட்டம், ஓய்வெடுக்க இயலாமை போன்ற உணர்வு)",
+            "பியர்ஸ். (இருள், அந்நியர்கள், தனிமை, விலங்குகள், கூட்டம், வாகனங்கள்)",
+            "இன்சோம்னியா. (தூங்குவதில் சிரமம், ஓய்வெடுக்காத இடைப்பட்ட தூக்கம், விழித்தவுடன் பலவீனம் மற்றும் பலவீனம் போன்ற உணர்வு, கனவுகள்)",
+            "அறிவுசார் குறைபாடு. (கவனம் செலுத்துவதில் சிரமம், நினைவகக் குறைபாடு)",
+            "மனச்சோர்வு மனநிலை. (பழக்கவழக்கங்களின் இழப்பு, ஒரு பொழுதுபோக்கிலிருந்து இன்ப உணர்வுகள், மனச்சோர்வு, ஆரம்ப விழிப்புணர்வு, தினசரி மனநிலை மாற்றங்கள்)",
+            "உடலுக்குரிய தசை அறிகுறிகள். (வலி, இழுத்தல், பதற்றம், குளோனிக் வலிப்பு, பற்களை அரைத்தல், கிழிந்த குரல், அதிகரித்த தசை தொனி)",
+            "உடலுக்குரிய (உணர்ச்சி). டின்னிடஸ், பார்வை மங்கலானது, சூடான மற்றும் குளிர்ச்சியான ஃப்ளஷ்கள், பலவீனத்தின் உணர்வுகள், முட்டாள்தனமான உணர்வு",
+            "இருதய அறிகுறிகள். (டாக்ரிக்கார்டியா, படபடப்பு, மார்பு வலி, பாத்திரங்களில் துடிப்பு, அடிக்கடி பெருமூச்சு)",
+            "சுவாச அறிகுறிகள். (மார்பு அழுத்தம் மற்றும் சுருக்கம், மூச்சுத் திணறல், அடிக்கடி பெருமூச்சு)",
+            "இரைப்பை குடல் அறிகுறிகள். (விழுங்குவதில் சிரமம், வாய்வு, வயிற்று வலி, நெஞ்செரிச்சல், வயிறு நிறைந்த உணர்வு, குமட்டல், வாந்தி, வயிற்றில் சலசலப்பு, வயிற்றுப்போக்கு, மலச்சிக்கல், எடை இழப்பு)",
+            "மரபணு அறிகுறிகள். (அடிக்கடி சிறுநீர் கழித்தல், வலுவான சிறுநீர் கழித்தல், மாதவிடாய், மாதவிடாய், சுறுசுறுப்பு, முன்கூட்டிய விந்துதள்ளல், ஆண்மை இழப்பு, ஆண்மைக் குறைவு)",
+            "தாவர அறிகுறிகள். (உலர்ந்த வாய், சிவத்தல் அல்லது சருமத்தின் வலி, வியர்வை, தலைவலி ஒரு பதற்றம் உணர்வுடன்)",
+            "ஆய்வு நடத்தை. (ஒரு நாற்காலியில் சறுக்குதல், அமைதியற்ற சைகை மற்றும் நடை, நடுக்கம், கோபம், பதட்டமான முகபாவங்கள், பெருமூச்சு அல்லது விரைவான சுவாசம், அடிக்கடி உமிழ்நீரை விழுங்குதல்)"
+        ]
+    },
+    "en": {
+        "answers": ["None", "Mild", "Moderate", "Severe", "Very severe"],
+        "questions": [
+            "Anxious. Worries, anticipation of the worst, fearful anticipation, irritability",
+            "Tension. Feelings of tension, fatigability, startle response, moved to tears easily, trembling, feelings of restlessness, inability to relax",
+            "Fears. Of dark, of strangers, of being left alone, of animals, of traffic, of crowds",
+            "Insomnia Difficulty in falling asleep, broken sleep, unsatisfying sleep and fatigue on waking, dreams, nightmares, night-terrors",
+            "Intellectual. Difficulty in concentration, poor memory",
+            "Depressed mood. Loss of interest, lack of pleasure in hobbies, depression, early waking, diurnal swing",
+            "Somatic (muscular). Pains and aches, twitching, stiffness, myoclonic jerks, grinding of teeth, unsteady voice, increased muscular tone",
+            "Somatic (sensory). Tinnitus, blurring of vision, hot and cold flushes, feelings of weakness, pricking sensation",
+            "Cardiovascular symptoms. Tachycardia, palpitations, pain in chest, throbbing of vessels, fainting feelings, missing beat",
+            "Respiratory symptoms. Pressure or constriction in chest, choking feelings, sighing, dyspnea",
+            "Gastrointestinal symptoms. Difficulty in swallowing, wind, abdominal pain, burning sensations, abdominal fullness, nausea, vomiting, borborygmi, looseness of bowels, loss of weight, constipation",
+            "Genitourinary symptoms. Frequency of micturition, urgency of micturition, amenorrhea, menorrhagia, development of frigidity, premature ejaculation, loss of libido, impotence",
+            "Autonomic symptoms. Dry mouth, flushing, pallor, tendency to sweat, giddiness, tension headache, raising of hair",
+            "Behavior at Interview. Fidgeting, restlessness or pacing, tremor of hands, furrowed brow, strained face, sighing or rapid respiration, facial pallor, swallowing, belching, brisk tendon jerks, dilated pupils, exophthalmos"
+        ]
+    }
+}
+
+
+MADRS_QUESTIONS = {
+    "ru": [
+        {
+            "question": "மனச்சோர்வின் குறிக்கோள் (தெரியும்) அறிகுறிகள். பேச்சு, முகபாவங்கள் மற்றும் தோரணையில் மனச்சோர்வு, விரக்தி, விரக்தி (வழக்கமான தற்காலிக மனநிலையை விட அதிகமாக வெளிப்படுகிறது). மனநிலை வீழ்ச்சியின் ஆழத்திற்கு ஏற்ப மதிப்பீடு செய்யப்படுகிறது.",
+            "answers": ["1 = சோகம் இல்லை", "2 =மனச்சோர்வடைந்ததாகத் தெரிகிறது, ஆனால் மனநிலை எளிதில் மேம்படும்", "3 = பெரும்பாலான நேரங்களில் மனச்சோர்வு மற்றும் பரிதாபமாக தெரிகிறது", "4 = எல்லா நேரத்திலும் மிகவும் மனச்சோர்விலும் மனச்சோர்விலும் தெரிகிறது"]
+        },
+        {
+            "question": "மனச்சோர்வின் அகநிலை அறிகுறிகள். வெளிப்புற அறிகுறிகளுடன் அது எவ்வாறு வெளிப்படுகிறது என்பதைப் பொருட்படுத்தாமல், மனச்சோர்வடைந்த மனநிலையைப் பற்றி நோயாளியைப் புகாரளித்தல். மனச்சோர்வு, மனச்சோர்வு அல்லது உதவியற்ற தன்மை மற்றும் நம்பிக்கையற்ற தன்மை ஆகியவை அடங்கும். நோயாளியின் விளக்கத்தின்படி, குறைக்கப்பட்ட மனநிலை வெளிப்புற நிகழ்வுகளுடன் தொடர்புடையது என்பதன் தீவிரம், காலம் மற்றும் அளவு ஆகியவற்றின் அடிப்படையில் இது மதிப்பீடு செய்யப்படுகிறது.",
+            "answers": ["1 = வெளிப்புற சூழ்நிலைகளுடன் தொடர்புடைய எபிசோடிக் மனச்சோர்வு", "2 = சோகமான அல்லது மனச்சோர்வடைந்த மனநிலை, முன்னேற்றத்திற்கு எளிதில் ஏற்றது", "3 = மனச்சோர்வு அல்லது விரக்தியின் ஆழமான உணர்வு; மனநிலை இன்னும் வெளிப்புற நிகழ்வுகளால் பாதிக்கப்படுகிறது", "4 = மனச்சோர்வு, விரக்தி அல்லது மனச்சோர்வு ஆகியவற்றின் நிலையான மற்றும் மாறாத உணர்வு"]
+        },
+        {
+            "question": "உள் மன அழுத்தம். வலி அச om கரியம், குழப்பம், எரிச்சல், மன அழுத்தம், பீதி, தீவிர பயம் அல்லது மன வலி போன்ற நிலையை அடைகிறது.",
+            "answers": ["1 = அமைதியான நிலை; உள் பதற்றம் ஒரு உணர்வு மட்டுமே", "2 = எப்போதாவது எரிச்சல் அல்லது வலி இருக்கிறது", "3 = உள் பதற்றத்தின் நிலையான உணர்வு, நோயாளியால் மிகுந்த சிரமத்துடன் அவ்வப்போது ஏற்படும் பீதி", "4 = இடைவிடாத தீவிர பயம் அல்லது மன வலி; தவிர்க்கமுடியாத பீதி"]
+        },
+        {
+            "question": "தூக்கமின்மை. நோயாளியின் தூக்கத்தின் வழக்கமான பண்புகளுடன் ஒப்பிடும்போது தூக்கத்தின் காலம் அல்லது ஆழத்தை குறைத்தல்.",
+            "answers": ["1 = சாதாரண கனவு", "2 = தூங்குவதில் சற்று சிரமம் அல்லது ஓரளவு சுருக்கப்பட்ட, ஆழமற்ற அல்லது இடைப்பட்ட தூக்கம்", "3 = குறுகிய தூக்கம், குறைந்தது 2 மணி நேரம்", "4 = 2-3 மணி நேரத்திற்கும் குறைவான தூக்கம்"]
+        },
+        {
+            "question": "பசியின்மை குறைகிறது. சாப்பிட ஆசை இழப்பு அல்லது உங்களை உண்ணும்படி கட்டாயப்படுத்தும் முயற்சிகளுக்கு ஏற்ப மதிப்பீடு செய்யப்படுகிறது.",
+            "answers": ["1 = சாதாரண அல்லது அதிகரித்த பசி", "2 = சற்று குறைக்கப்பட்ட பசி", "3 =பசியின்மை; உணவுக்கு சுவை இல்லை", "4 = சாப்பிட வற்புறுத்தலின் தேவை"]
+        },
+        {
+            "question": "பலவீனமான செறிவு. கவனம் செலுத்தும் திறனை இழக்கும் வரை எண்ணங்களைச் சேகரிப்பதில் சிரமங்கள். கவனம் செலுத்தும் திறனை இழப்பதன் தீவிரம், அதிர்வெண் மற்றும் அளவிற்கு ஏற்ப மதிப்பீடு செய்யப்படுகிறது.",
+            "answers": ["1 = செறிவில் எந்த இடையூறும் இல்லை", "2 = எண்ணங்களைச் சேகரிப்பது அவ்வப்போது கடினம்", "3 = உரையாடலைப் படிக்க அல்லது பராமரிக்கும் திறனுடன் குறைவான கவனம் செலுத்துதல் மற்றும் நீடித்த செறிவு", "4 = குறிப்பிடத்தக்க முயற்சி இல்லாமல் உரையாடலைப் படிக்க அல்லது பங்கேற்கும் திறனை இழத்தல்"]
+        },
+        {
+            "question": "அக்கறையின்மை. எந்தவொரு செயலையும் தொடங்குவதில் உள்ள சிரமங்கள் அல்லது தொடக்கத்தை மெதுவாக்குவது மற்றும் அன்றாட நடவடிக்கைகளை நிறைவேற்றுவது.",
+            "answers": ["1 = எந்தவொரு செயலையும் தொடங்க சிரமம் இல்லாதது; மந்தநிலை இல்லாமை", "2 = ஒரு செயல்பாட்டைத் தொடங்குவதில் சிரமம்", "3 = எளிய தினசரி நடவடிக்கைகளைத் தொடங்குவதில் உள்ள சிரமங்கள், அவற்றைச் செயல்படுத்த கூடுதல் முயற்சி தேவைப்படுகிறது", "4 = முழுமையான அக்கறையின்மை; உதவி இல்லாமல் எதையும் செய்ய இயலாமை"]
+        },
+        {
+            "question": "உணரும் திறன் இழப்பு. சுற்றுச்சூழல் அல்லது செயல்பாட்டில் ஆர்வம் குறைவதற்கான அகநிலை உணர்வு, பொதுவாக ஒரு இன்பம். வெளிப்புற நிகழ்வுகள் அல்லது நபர்களுக்கு போதுமான உணர்ச்சிபூர்வமாக பதிலளிக்கும் திறன் குறைகிறது",
+            "answers": ["1 = சுற்றுச்சூழல் மற்றும் மக்கள் மீது சாதாரண ஆர்வம்", "2 = பொதுவாக சுவாரஸ்யமானவற்றை அனுபவிக்கும் திறன் குறைந்தது", "3 = சூழலில் ஆர்வம் இழப்பு; நண்பர்கள் மற்றும் அறிமுகமானவர்களுக்கு உணர்வுகளை இழத்தல்", "4 = உணர்ச்சி முடக்குதல், கோபம், சோகம் அல்லது இன்பத்தை அனுபவிக்கும் திறனை இழத்தல், உறவினர்கள் மற்றும் நண்பர்களுக்கு முழுமையான அல்லது வேதனையான உணர்வுகளை இழத்தல்"]
+        },
+        {
+            "question": "அவநம்பிக்கையான எண்ணங்கள். ஒருவரின் சொந்த குற்றவுணர்வு, பயனற்ற தன்மை, சுய இழிவு, பாவம் அல்லது மனந்திரும்புதல் போன்ற கருத்துக்கள்",
+            "answers": ["1 = அவநம்பிக்கையான எண்ணங்கள் இல்லாமை", "2 = வாழ்க்கை தோல்வி, சுய இழிவு அல்லது குறைந்த மதிப்பு பற்றிய எபிசோடிக் கருத்துக்கள்", "3 = நிலையான சுய-குற்றச்சாட்டு அல்லது குறிப்பிட்ட, ஆனால் இன்னும் பகுத்தறிவு, குற்ற உணர்வு அல்லது பாவத்தின் கருத்துக்கள்; எதிர்காலத்தின் வளர்ந்து வரும் அவநம்பிக்கை மதிப்பீடு", "4 = முழுமையான சரிவு, வருத்தம் அல்லது தவிர்க்கமுடியாத பாவத்தின் பிரமைகள்; அபத்தமான மற்றும் அசைக்க முடியாத சுய-குற்றம்"]
+        },
+        {
+            "question": "தற்கொலை எண்ணங்கள். வாழ்வது இனி பயனில்லை, இயற்கை மரணம் தான் விரும்பிய விளைவு; தற்கொலை எண்ணங்கள் மற்றும் தற்கொலைக்கான ஏற்பாடுகள்.",
+            "answers": ["1 = வாழ்க்கை இன்பத்தைத் தருகிறது அல்லது அது போலவே உணரப்படுகிறது", "2 = வாழ்க்கையிலிருந்து சோர்வு; தற்கொலை எண்ணங்கள்", "3 = ஒருவேளை இறப்பது நல்லது; தற்கொலை எண்ணங்கள் பழக்கமாகின்றன, மேலும் தற்கொலை என்பது குறிப்பிட்ட தற்கொலை திட்டங்கள் அல்லது நோக்கங்கள் இல்லாத நிலையில் சிக்கல்களை தீர்க்க ஒரு சாத்தியமான வழியாக கருதப்படுகிறது", "4 = குறிப்பிட்ட தற்கொலை திட்டமிடல் விரைவில்; செயலில் தற்கொலை ஏற்பாடுகள்"]
+        }
+    ],
+    "en": [
+        {
+            "question": "Apparent Sadness. Representing despondency, gloom and despair, (more than just ordinary transient low spirits) reflected in speech, facial expression, and posture. Rate by depth and inability to brighten up",
+            "answers": ["1 = No sadness", "2 = Looks dispirited but does brighten up without difficulty", "3 = Appears sad and unhappy most of the time", "4 = Looks miserable all the time. Extremely despondent"]
+        },
+        {
+            "question": "Reported Sadness. Representing reports of depressed mood, regardless of whether it is reflected in appearance or not. Includes low spirits, despondency or the feeling of being beyond help and without hope. Rate according to intensity, duration and the extent to which the mood is reported to be influenced by events",
+            "answers": ["1 = Occasional sadness in keeping with the circumstances", "2 = Sad or low but brightens up without difficulty", "3 = Pervasive feelings of sadness or gloominess. The mood is still influenced by external circumstances", "4 = Continuous or unvarying sadness, misery or despondency"]
+        },
+        {
+            "question": "Inner Tension. Representing feelings of ill-defined discomfort, edginess, inner turmoil, mental tension mounting to either panic, dread or anguish. Rate according to intensity, frequency, duration and the extent of reassurance called for",
+            "answers": ["1 = Placid. Only fleeting inner tension", "2 = Occasional feelings of edginess and ill-defined discomfort", "3 = Continuous feelings of inner tension or intermittent panic which the patient can only master with some difficulty", "4 = Unrelenting dread or anguish. Overwhelming panic"]
+        },
+        {
+            "question": "Reduced Sleep. Representing the experience of reduced duration or depth of sleep compared to the subject’s own normal pattern when well",
+            "answers": ["1 = Sleeps as usual", "2 = Slight difficulty dropping off to sleep or slightly reduced, light or fitful sleep", "3 = Sleep reduced or broken by at least two hours", "4 = Less than two or three hours sleep"]
+        },
+        {
+            "question": "Reduced Appetite. Representing the feeling of a loss of appetite compared with when well. Rate by loss of desire for food or the need to force oneself to eat",
+            "answers": ["1 = Normal or increased appetite", "2 = Slightly reduced appetite", "3 = No appetite. Food is tasteless", "4 = Needs persuasion to eat at all"]
+        },
+        {
+            "question": "Concentration Difficulties. Representing difficulties in collecting one’s thoughts mounting to incapacitating lack of concentration. Rate according to intensity, frequency, and degree of incapacity produced",
+            "answers": ["1 = No difficulties in concentrating", "2 = Occasional difficulties in collecting one’s thoughts", "3 = Difficulties in concentrating and sustaining thought which reduces ability to read or hold a conversation", "4 = Unable to read or converse without great difficulty"]
+        },
+        {
+            "question": "Lassitude. Representing a difficulty getting started or slowness initiating and performing everyday activities",
+            "answers": ["1 = Hardly any difficulties in getting started. No sluggishness", "2 = Difficulties in starting activities", "3 = Difficulties in starting simple routine activities, which are carried out with effort", "4 = Complete lassitude. Unable to do anything without help"]
+        },
+        {
+            "question": "Inability to Feel. Representing the subjective experience of reduced interest in the surroundings, or activities that normally give pleasure.The ability to react with adequate emotion to circumstances or people is reduced",
+            "answers": ["1 = Normal interest in the surroundings and in other people", "2 = Reduced ability to enjoy usual interests", "3 = Loss of interest in the surroundings. Loss of feelings for friends and acquaintances", "4 = The experience of being emotionally paralyzed, inability to feel anger, grief or pleasure and a complete or even painful failure to feel for close relatives and friends"]
+        },
+        {
+            "question": "Pessimistic Thoughts. Representing thoughts of guilt, inferiority, self-reproach, sinfulness, remorse and ruin",
+            "answers": ["1 = No pessimistic thoughts", "2 = Fluctuating ideas of failure, self-reproach or self-depreciation", "3 = Persistent self-accusations, or definite but still rational ideas of guilt or sin. Increasingly pessimistic about the future", "4 = Delusions of ruin, remorse and unredeemable sin. Self-accusations which are absurd and unshakable"]
+        },
+        {
+            "question": "Suicidal Thoughts. Representing the feeling that life is not worth living, that a natural death would be welcome, suicidal thoughts, and preparations for suicide. Suicidal attempts should not in themselves influence the rating",
+            "answers": ["1 = Enjoys life or takes it as it comes", "2 = Weary of life. Only fleeting suicidal thoughts", "3 = Probably better off dead. Suicidal thoughts are common, and suicide is considered as a possible solution, but without specific plans or intention", "4 = Explicit plans for suicide when there is an opportunity. Active preparations for suicide"]
+        }
+    ]
+}
